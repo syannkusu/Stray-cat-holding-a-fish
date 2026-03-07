418fb58704724a3aaf2804d9bd307788aeb059e0
syannkusu <ishiryouhei0219@icloud.com>
2026-03-08 07:50:55 +0900

    JS1回目の保存

---
+++ MyScript.js
@@ -0,0 +1,10 @@
+<script>
+    function addItem(rate){
+        let name=document.getELementById("name").value
+        let file=document.getELementById("camera").files[0]
+        if(!name){
+            alert("商品名を入力")
+            return
+  }
+    }
+</script>
