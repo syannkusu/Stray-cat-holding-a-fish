e43c2d23d38c10675f91fa817aba1bdc2ea2716e
syannkusu <ishiryouhei0219@icloud.com>
2026-03-08 07:56:28 +0900

    JS2回目の保存

--- MyScript.js
+++ MyScript.js
@@ -1,10 +1,16 @@
 <script>
     function addItem(rate){
-        let name=document.getELementById("name").value
-        let file=document.getELementById("camera").files[0]
+        // Lを小文字に修正
+        let name = document.getElementById("name").value;
+        let file = document.getElementById("camera").files[0];
+        
         if(!name){
-            alert("商品名を入力")
-            return
-  }
+            alert("商品名を入力");
+            return;
+        }
+        
+        // この後に、選んだ「◎⚪︎△✖️」の評価(rate)を保存する処理などを続けます
+        console.log("商品名:", name);
+        console.log("評価:", rate);
     }
 </script>
