29226cab2965f3ac09442be5b3f9b7890c476e9d
syannkusu <ishiryouhei0219@icloud.com>
2026-03-12 07:45:17 +0900

    接続

--- app.html/アプリケーション/my-project/index.php
+++ app.html/アプリケーション/my-project/index.php
@@ -1,1 +1,17 @@
 http://localhost/my_project/index.php
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "root"; // MAMPのデフォルトパスワード
+$dbname = "test_db"; // あなたが作ったデータベース名
+
+try {
+    // PDOという仕組みを使って接続します
+    $dsn = "mysql:host={$host};dbname={$dbname};charset=utf8mb4";
+    $pdo = new PDO($dsn, $user, $pass);
+    
+    echo "接続に成功しました！";
+} catch (PDOException $e) {
+    echo "接続エラー: " . $e->getMessage();
+}
+?>
